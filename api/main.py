9dfcaff31f920bf4b95161660ea45cd6a8b059f3
vaibhav-vemula <vaibhav25vemula23@gmail.com>
2025-11-05 16:35:38 -0500

    fixed cors

--- api/main.py
+++ api/main.py
@@ -58,8 +58,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://*.vercel.app",
-        "https://parkpulse.vercel.app", 
+        "https://parkpulseai.vercel.app",
+        "https://parkpulse.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
